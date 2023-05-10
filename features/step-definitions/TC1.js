598b76b2d8600adb427b4871ea25f61daaa8a216
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2023-05-10 17:37:32 +0100

    Update TC1.js

--- features/step-definitions/TC1.js
+++ features/step-definitions/TC1.js
@@ -24,20 +24,25 @@ let driver = new webdriver.Builder()
 Given('I visit publicazo homepage', {timeout: 15 * 1000}, async () => {    
     
     await driver.get("http://publicazo.insprak.com/")
+    await driver.manage().window().setRect({ width: 1266, height: 656 })
     
     
           });
 
 When('I search for teste', async () => {
+    await driver.findElement(By.id("search")).click()
     await driver.findElement(By.id("search")).sendKeys("teste")
     await driver.findElement(By.name("commit")).click()
+
           });
 
 Then('I should see the results', async () => {
     
-    console.log('fim')
+    await driver.findElement(By.css(".col-md-4:nth-child(3) .panel-body")).click()
+    assert(await driver.findElement(By.linkText("Aulas de Teste de Software")).getText() == "Aulas de Teste de Software")
+    await driver.close();
     //===============
 
-    await driver.quit()
+    await driver.quit();
     
           });
